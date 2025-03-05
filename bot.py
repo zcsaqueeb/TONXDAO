8be902604c6c771b7ec95d0e025ab74350d67570
I AM CONFFIN <saqueebbegumpure@gmail.com>
2025-03-05 12:28:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 import sys
+import base64
+import requests
+import json
+import time
 
 sys.dont_write_bytecode = True
 
@@ -7,11 +11,6 @@ from core.token import get_token, get_centrifugo_token
 from core.info import get_info
 from core.task import process_check_in, process_do_task
 from core.ws import process_farm
-import base64
-import requests
-import json
-import time
-
 
 def key_bot():
     api = base64.b64decode("aHR0cDovL2l0YmFhcnRzLmNvbS9hcGkuanNvbg==").decode('utf-8')
@@ -29,29 +28,13 @@ def key_bot():
 
 class TONxDAO:
     def __init__(self):
-        # Get file directory
         self.data_file = base.file_path(file_name="data.txt")
         self.config_file = base.file_path(file_name="config.json")
-
-        # Initialize line
         self.line = base.create_line(length=50)
-
-        # Get config
-        self.auto_check_in = base.get_config(
-            config_file=self.config_file, config_name="auto-check-in"
-        )
-
-        self.auto_do_task = base.get_config(
-            config_file=self.config_file, config_name="auto-do-task"
-        )
-
-        self.auto_claim_ref = base.get_config(
-            config_file=self.config_file, config_name="auto-claim-ref"
-        )
-
-        self.auto_farm = base.get_config(
-            config_file=self.config_file, config_name="auto-farm"
-        )
+        self.auto_check_in = base.get_config(self.config_file, "auto-check-in")
+        self.auto_do_task = base.get_config(self.config_file, "auto-do-task")
+        self.auto_claim_ref = base.get_config(self.config_file, "auto-claim-ref")
+        self.auto_farm = base.get_config(self.config_file, "auto-farm")
 
     def main(self):
         while True:
@@ -65,49 +48,40 @@ class TONxDAO:
             for no, data in enumerate(data):
                 base.log(self.line)
                 base.log(f"{base.green}Account number: {base.white}{no+1}/{num_acc}")
-
                 try:
                     token = get_token(data=data)
-
                     if token:
-
                         dao_id = get_info(token=token)
-
                         centrifugo_token = get_centrifugo_token(token=token)
-
-                        # Check in
+                        
                         if self.auto_check_in:
                             base.log(f"{base.yellow}Auto Check-in: {base.green}ON")
                             process_check_in(token=token)
                         else:
                             base.log(f"{base.yellow}Auto Check-in: {base.red}OFF")
-
-                        # Do task
+                        
                         if self.auto_do_task:
                             base.log(f"{base.yellow}Auto Do Task: {base.green}ON")
                             process_do_task(token=token)
                         else:
                             base.log(f"{base.yellow}Auto Do Task: {base.red}OFF")
-
-                        # Farm
+                        
                         if self.auto_farm:
                             base.log(f"{base.yellow}Auto Farm: {base.green}ON")
                             process_farm(token=centrifugo_token, dao_id=dao_id)
                         else:
                             base.log(f"{base.yellow}Auto Farm: {base.red}OFF")
-
+                        
                         get_info(token=token)
-
                     else:
                         base.log(f"{base.red}Token not found! Please get new query id")
                 except Exception as e:
                     base.log(f"{base.red}Error: {base.white}{e}")
 
             print()
-            wait_time = 60 * 60
-            base.log(f"{base.yellow}Wait for {int(wait_time/60)} minutes!")
+            wait_time = 60 * 60 * 7  # 7 hours
+            base.log(f"{base.yellow}Wait for {int(wait_time/3600)} hours!")
             time.sleep(wait_time)
-
 
 if __name__ == "__main__":
     try:
